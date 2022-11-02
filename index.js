d941df30b4b7b445ac70d211047665fe22a393a9
lizdecoder <eys.1908@gmail.com>
2022-11-02 17:12:25 -0400

    updated to include forms

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const timeInput = document.querySelector('.timeInput');
-const display = document.querySelector('.display');
-const date = document.querySelector('h5').innerText;
+// const timeInput = document.querySelector('.timeInput');
+const date = document.querySelector('.date');
+const title = document.querySelector('h3');
+const scale = document.querySelector('#scaleType');
+const para = document.createElement("p");
+
+// console.log(title.value)
+// const date = document.querySelector('.date');
+// const scaleType = document.querySelector('.date');
+// const date = document.querySelector('#date').innerText;
+// const scaleType = document.getElementById('scaleType');
+
+const form = document.querySelector('form');
 
 const debounce = (func, delay=1000) => {
     let timeoutID;
@@ -16,9 +26,36 @@ const debounce = (func, delay=1000) => {
 
 const onInput = (e) => {
     const inputValue = e.target.value;
-    display.innerHTML = `<p><b>Recorded Complete Time</b>: ${inputValue}</p>`;
-    timeInput.classList.add('d-none');
+    console.log(e.target.value);
+    title.innerHTML = `${inputValue}`;
+    scale.innerHTML = `${inputValue}`;
+    
+    // display.innerHTML = `<p><b>${inputValue}</p>`;
+    date.classList.add('hidden');
 };
 
+// timeInput.addEventListener('input', debounce(onInput), 500);
+form.addEventListener('input', debounce(onInput), 500);
+// title.addEventListener('input', debounce(onInput), 500);
 
-timeInput.addEventListener('input', debounce(onInput), 500);
+
+// if (message) {
+//     document.querySelector('#message-form').classList.add('hide');
+//     document.querySelector('#message-show').classList.remove('hide');
+
+//     document.querySelector('h1').innerHTML = message;
+// }
+
+// document.querySelector('form').addEventListener('submit', event => {
+//     event.preventDefault();
+
+//     document.querySelector('#message-form').classList.add('hide');
+//     document.querySelector('#link-form').classList.remove('hide');
+
+//     const input = document.querySelector('#message-input');
+//     const encrypted = btoa(input.value);
+    
+//     const linkInput = document.querySelector('#link-input');
+//     linkInput.value = `${window.location}#${encrypted}`;
+//     linkInput.select()
+// });
